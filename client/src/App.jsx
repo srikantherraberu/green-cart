a9790db297a779d0f700a66d33c2ed72087725e4
Srikanth Erraberu <srikantherraberu@gmail.com>
2026-02-09 22:33:11 +0530

    Updated Seller Folder with some Pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,6 +14,9 @@ import AddAddress from './pages/AddAddress';
 import MyOrders from './pages/MyOrders';
 import SellerLogin from './components/seller/SellerLogin';
 import SellerLayout from './pages/seller/SellerLayout';
+import AddProduct from './pages/seller/AddProduct';
+import ProductList from './pages/seller/ProductList';
+import Orders from './pages/seller/Orders';
 
 
 const App = () => {
@@ -35,7 +38,11 @@ const App = () => {
           <Route path='/cart' element={<Cart/>}/>
           <Route path='/add-address' element={<AddAddress/>}/>
           <Route path='/my-orders' element={<MyOrders/>}/>
-          <Route path='/seller' element={isSeller? <SellerLayout/> : <SellerLogin/>}></Route>
+          <Route path='/seller' element={isSeller? <SellerLayout/> : <SellerLogin/>}>
+            <Route index element={isSeller ? <AddProduct/> : null } />
+            <Route path="product-list" element={<ProductList/> } />
+            <Route path="orders" element={<Orders/>} />
+          </Route>
         </Routes>
       </div>
       {!isSellerPath && <Footer/>}
