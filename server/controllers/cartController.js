8332cf26528a705bc1c80bcc602120347f37e9df
Srikanth Erraberu <srikantherraberu@gmail.com>
2026-02-20 02:19:38 +0530

    Worked on Address, Cart, Order Controlling and Routing.

---
+++ server/controllers/cartController.js
@@ -0,0 +1,16 @@
+import User from "../models/User.js";
+
+
+//Update User cartData: /api/cart/update
+
+
+export const updateCart= async(req,res)=>{
+    try {
+        const {userId, cartItems}= req.body;
+        await User.findByIdAndUpdate(userId, {cartItems});
+        return res.json({success: true, message: "Cart Updated"});
+    } catch (error) {
+        console.log(error.message);
+        return res.json({success: false, message: error.message });
+    }
+}
