9a02a032a3a16acbb97ee46ac8504a42ff6af007
Srikanth Erraberu <srikantherraberu@gmail.com>
2026-03-07 03:12:56 +0530

    fix case sensitive import

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,7 @@ import {Toaster} from 'react-hot-toast';
 import Footer from './components/Footer';
 import { useAppContext } from './context/AppContext';
 import Login from './components/Login';
-import AllProducts from './pages/Allproducts';
+import AllProducts from './pages/AllProducts';
 import ProductCategory from './pages/ProductCategory';
 import ProductDetails from './pages/ProductDetails';
 import Cart from './pages/Cart';
